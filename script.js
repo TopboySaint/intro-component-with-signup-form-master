0bb8963eb633b601ac1a492411763d10b84964fc
TopboySaint <topboysaint1@gmail.com>
2025-02-09 20:34:19 +0100

    Enhance sign-up form validation and error handling with improved messaging and styles

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+document.getElementById('errorMsg').style.display = "none";
+document.getElementById('errorMsgTwo').style.display = "none";
+document.getElementById('errorMsgThree').style.display = "none";
+document.getElementById('errorMsgFour').style.display = "none";
+
 const submitForm = () => {
-    alert('Form submitted');
-    if(document.getElementById('fname').value === '' || document.getElementById('lname').value === '' || document.getElementById('').value === '') {
-        // alert('Please fill in all fields');
+    event.preventDefault();
+
+    if (document.getElementById('fname').value === '') {
         document.getElementById('errorMsg').style.display = 'block';
+        document.getElementById('fname').style.border = '2px solid red';
+    } else if (document.getElementById('lname').value === '') {
+        document.getElementById('errorMsgTwo').style.display = 'block';
+        document.getElementById('lname').style.border = '2px solid red';
+    } else if (document.getElementById('email').value === '') {
+        document.getElementById('errorMsgThree').style.display = 'block';
+        document.getElementById('email').style.border = '2px solid red';
+    } else if (document.getElementById('password').value === '') {
+        document.getElementById('errorMsgFour').style.display = 'block';
+        document.getElementById('password').style.border = '2px solid red';
     }
 }
